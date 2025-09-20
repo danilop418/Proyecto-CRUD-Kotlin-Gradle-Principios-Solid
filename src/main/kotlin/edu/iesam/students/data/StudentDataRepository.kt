5fe924453929962a73fb746a59de2aa8030e9488
danilop418 <daniel.lopgon.4@educa.jcyl.es>
2025-09-20 11:16:10 +0200

    Commit 2 : Añado comentarios a StudentDataRepository para revisar mañana

--- src/main/kotlin/edu/iesam/students/data/StudentDataRepository.kt
+++ src/main/kotlin/edu/iesam/students/data/StudentDataRepository.kt
@@ -6,17 +6,20 @@ import edu.iesam.students.domain.StudentRepository
 import edu.iesam.students.domain.data.local.remote.StudentMemLocalDataSource
 import edu.iesam.students.domain.data.local.remote.StudentXmlLocalDataSource
 
-class StudentDataRepository (
+class StudentDataRepository(
     private val xmlLocalDataSource: StudentXmlLocalDataSource,
     private val memLocalDataSource: StudentMemLocalDataSource,
     private val apiRemoteDataSource: StudentApiRemoteDataSource
 ) : StudentRepository {
 
     override fun save(student: Student) {
+        /*
         if(memLocalDataSource.exist(student)){
 
         } else {
 
         }
+    }
+         */
     }
 }
