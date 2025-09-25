c49f2f9f35d679aea360b8c1e7d92f010942154f
danilop418 <daniel.lopgon.4@educa.jcyl.es>
2025-09-25 11:43:45 +0200

    commit : fix isEmpty

--- src/main/kotlin/edu/iesam/students/domain/SaveStudentUseCase.kt
+++ src/main/kotlin/edu/iesam/students/domain/SaveStudentUseCase.kt
@@ -3,10 +3,10 @@ package edu.iesam.students.domain
 class SaveStudentUseCase(val studentRepository: StudentRepository) {
 
     operator fun invoke(student: Student) {
-        if (!student.name.isEmpty()) {
+        if (student.name.isEmpty()) {
             throw IllegalArgumentException("Student name cannot empty")
         }
-        if (!student.exp.isEmpty()) {
+        if (student.exp.isEmpty()) {
             throw IllegalArgumentException("Student exp cannot empty")
         }
         studentRepository.save(student)
